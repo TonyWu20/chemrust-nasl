0738b845d094bb64be1506b18086a058ca47f7af
TonyWu20 <tony.w21@gmail.com>
2024-05-07 17:50:44 +0800

    Changed: Plane-Plane intersection result changes to three cases

--- src/analysis/geometry/intersections/plane_plane.rs
+++ src/analysis/geometry/intersections/plane_plane.rs
@@ -4,31 +4,51 @@ use nalgebra::{Point3, UnitVector3};
 
 use crate::analysis::geometry::primitives::{Line, Plane};
 
-pub fn plane_plane_intersection(p1: &Plane, p2: &Plane) -> Option<Line> {
-    let n1 = p1.normal();
-    let n2 = p2.normal();
-    let n3 = n1.cross(&n2);
-    if n3.norm_squared().abs() < EPSILON {
-        return None;
+use super::{approx_cmp_f64, FloatOrdering, Intersect};
+
+#[derive(Debug, Clone, Copy)]
+pub enum PlanePlaneIntersection {
+    Same,
+    Parallel,
+    Intersect(Line),
+}
+
+impl Intersect for Plane {
+    type Output = PlanePlaneIntersection;
+
+    fn intersect(&self, rhs: &Self) -> Self::Output {
+        let n1 = self.normal();
+        let n2 = rhs.normal();
+        let n3 = n1.cross(&n2);
+        if n3.norm_squared() < EPSILON {
+            let d1 = self.d();
+            let d2 = self.d();
+            if let FloatOrdering::Equal = approx_cmp_f64(d1, d2) {
+                PlanePlaneIntersection::Same
+            } else {
+                PlanePlaneIntersection::Parallel
+            }
+        } else {
+            let h1 = -self.d();
+            let h2 = -rhs.d();
+            let d1 = (h1 - h2 * n1.dot(&n2)) / (1.0 - n1.dot(&n2).powi(2));
+            let d2 = (h2 - h1 * n1.dot(&n2)) / (1.0 - n1.dot(&n2).powi(2));
+            let x0 = Point3::from(n1.scale(d1) + n2.scale(d2));
+            let line = Line::new(x0, UnitVector3::new_normalize(n3));
+            PlanePlaneIntersection::Intersect(line)
+        }
     }
-    let h1 = -p1.d();
-    let h2 = -p2.d();
-    let d1 = (h1 - h2 * n1.dot(&n2)) / (1.0 - n1.dot(&n2).powi(2));
-    let d2 = (h2 - h1 * n1.dot(&n2)) / (1.0 - n1.dot(&n2).powi(2));
-    let x0 = Point3::from(n1.scale(d1) + n2.scale(d2));
-    let line = Line::new(x0, UnitVector3::new_normalize(n3));
-    Some(line)
 }
 
 #[cfg(test)]
-mod circle_circle {
-    use std::f64::EPSILON;
+mod test {
 
-    use nalgebra::{Matrix3x2, Point3, UnitVector3, Vector2, Vector3};
+    use nalgebra::{Point3, UnitVector3, Vector3};
 
-    use crate::analysis::geometry::primitives::{Circle3d, Plane};
-
-    use super::plane_plane_intersection;
+    use crate::analysis::geometry::{
+        intersections::{plane_plane::PlanePlaneIntersection, Intersect},
+        primitives::{Circle3d, Plane},
+    };
 
     #[test]
     fn plane_plane() {
@@ -44,15 +64,24 @@ mod circle_circle {
         );
         let plane_1 = c1.plane_of_circle();
         let plane_2 = c2.plane_of_circle();
-        let p_p = plane_plane_intersection(&plane_1, &plane_2);
-        println!("{:?}", p_p);
+        if let PlanePlaneIntersection::Intersect(line) = plane_1.intersect(&plane_2) {
+            let origin = line.origin();
+            let d = origin - c1.center();
+            let ld = line.direction();
+            let angle = d.angle(&ld);
+            println!("{:?}", line);
+            println!("{}", d);
+            println!("{}", angle);
+            let h = d.norm() * angle.sin();
+            println!("{}, {}", h, d.norm());
+        }
         let plane_3 = Plane::new(UnitVector3::new_normalize(Vector3::new(1.0, 0.0, 0.0)), 3.0);
         let plane_4 = Plane::new(UnitVector3::new_normalize(Vector3::new(0.0, 1.0, 0.0)), 6.0);
-        let p_p = plane_plane_intersection(&plane_3, &plane_4);
+        let p_p = plane_3.intersect(&plane_4);
         println!("{:?}", p_p);
         let plane_5 = Plane::new(UnitVector3::new_normalize(Vector3::new(1.0, 0.0, 0.0)), 3.0);
         let plane_6 = Plane::new(UnitVector3::new_normalize(Vector3::new(1.0, 0.0, 0.0)), 6.0);
-        let p_p = plane_plane_intersection(&plane_5, &plane_6);
+        let p_p = plane_5.intersect(&plane_6);
         println!("{:?}", p_p);
     }
     #[test]
