7be0ae07a2cef95b7b3897717e0c3d9b3e463fec
TonyWu20 <tony.w21@gmail.com>
2024-11-08 18:42:54 +0800

    Fix wrong merge

--- chemrust-nasl/src/lib.rs
+++ chemrust-nasl/src/lib.rs
@@ -2,6 +2,6 @@ mod algorithm;
 mod coordination_sites;
 mod geometry;
 
-pub use algorithm::{AdsSiteLocator, SearchConfig, SearchReports, SiteIndex};
+pub use algorithm::{search_sites, SearchConfig, SearchReports, SiteIndex};
 pub use coordination_sites::*;
 pub use geometry::*;
