fa8f0578dccfb054dd0a3e5abbd033092f284994
TonyWu20 <tony.w21@gmail.com>
2024-12-24 11:37:42 +0800

    Use `thiserror` to impl error types

--- chemrust-nasl-app/src/error.rs
+++ chemrust-nasl-app/src/error.rs
@@ -1,45 +1,25 @@
-use std::fmt::Display;
+use thiserror::Error;
 
-#[derive(Debug)]
+#[derive(Error, Debug)]
 pub enum RunError {
+    #[error("{0}")]
     Message(String),
-    FormatError(FormatError),
+    #[error("{0}")]
+    FormatError(#[from] FormatError),
+    #[error("Error in IO operations")]
     IO,
 }
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Error, Debug, Clone, Copy)]
 pub enum FormatError {
+    #[error("The file does not have extension suffix `.xxx`")]
     Identified,
+    #[error("This format is not supported")]
     Supported,
+    #[error("The file content is not compatible with the designated format")]
     Compatible,
+    #[error("Failed while reading to string")]
     ReadToString,
+    #[error("Required parts are missing")]
+    Missing,
 }
-
-impl Display for FormatError {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            FormatError::Supported => f.write_str("This format is not supported"),
-            FormatError::Compatible => {
-                f.write_str("The file content is not compatible with the format requirement")
-            }
-
-            FormatError::Identified => {
-                f.write_str("The file does not have extension suffix `.xxx`")
-            }
-            FormatError::ReadToString => f.write_str("Failed to read to string"),
-        }
-    }
-}
-
-impl Display for RunError {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            RunError::Message(m) => f.write_str(m),
-            RunError::FormatError(m) => write!(f, "{m}"),
-            RunError::IO => f.write_str("Error in IO operations"),
-        }
-    }
-}
-
-impl std::error::Error for RunError {}
-impl std::error::Error for FormatError {}
