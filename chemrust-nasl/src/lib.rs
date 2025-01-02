1ed246381303dc02ef111d6179608abb8fc25f47
TonyWu20 <tony.w21@gmail.com>
2025-01-02 18:53:26 +0800

    Adjust namespace

--- chemrust-nasl/src/lib.rs
+++ chemrust-nasl/src/lib.rs
@@ -1,7 +1,8 @@
 mod algorithm;
 mod coordination_sites;
+mod execution;
 mod geometry;
 
-pub use algorithm::{SearchConfig, SearchReports};
 pub use coordination_sites::*;
+pub use execution::search_config::{SearchConfig, SearchReports};
 pub use geometry::*;
